8da4b5433331d1e097bd9a95bae723a856a43ce5
Ultrasss <eg0_mpx@mail.ru>
2022-07-25 08:49:45 +0300

    3day

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 $(function(){
     $('.music-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        arrows: true,
-        dots: true,
 
     });
     
