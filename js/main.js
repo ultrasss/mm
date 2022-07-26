59140320d5a5badd5017bd1fe83e67eab66042bf
Ultrasss <ego_mpx@mail.ru>
2022-07-26 17:29:45 +0300

    day5

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function(){
     $('.music-slider').slick({
-              
+        arrows: true,
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1       
     });
     
 });
