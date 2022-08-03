a4fbcdd29fbee30f08b15a31f09ef0873f7606db
Ultrasss <ego_mpx@mail.ru>
2022-08-03 16:35:02 +0300

    day11

--- js/form.js
+++ js/form.js
@@ -8,6 +8,29 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
 
         let error = formValidate(form);
+
+        let formData = new FormData(form);
+        formData.append('image', formImage.files[0]);
+
+        if (error === 0) {
+            form.classicList.add('_sending'); 
+             let response = await fetch ('sendmail.php', {
+                method: 'POST',
+                body: formData
+             });
+             if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+                form.classList.remove('_sending');
+             } else { 
+                alert("Ошибка");
+                form.classList.remove('_sending');
+             }
+                } else {
+            alert('Заполните обязательные поля');
+        }
     }
 
 
@@ -15,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let error = 0;
         let formReq = document.querySelectorAll('._req');
 
-        for (let index = 0; index < formReq.lenght; index++) {
+        for (let index = 0; index < formReq.length; index++) {
             const input = formReq[index];
             formRemoveError(input);
 
@@ -35,6 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
         }
+        return error;
     }
     function formAddError(input) {
         input.parentElement.classList.add('_error');
@@ -49,5 +73,36 @@ document.addEventListener('DOMContentLoaded', function() {
         return !/^\w+([\.-]?\w+)*@w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
     }
     
+    //Получаем инпут file в переменную
+    const formImage = document.getElementById('formImage');
+    //Получаем див для превью в переменную
+    const formPreview = document.getElementById('formPreview');
     
-});
+    //Слушаем изменения в инпуте file
+    formImage.addEventListener('change', () => {
+       uploadFile(formImage.files[0]); 
+    });
+
+    function uploadFile(file) {
+        //проверяем тип файла
+        if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+            alert('Разрешены только изображения.');
+            formImage.value = '';
+            return;
+        }
+        //проверяем размер файла (<2 Мб)
+        if (file.size > 2 * 1024 * 1024) {
+            alert('Файл должен быть менее 2 МБ.');
+            return;
+        }
+
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+        };
+        reader.onerror = function (e) {
+            alert('Ошибка');
+        };
+        reader.readAsDataURL(file);
+        }
+        });
